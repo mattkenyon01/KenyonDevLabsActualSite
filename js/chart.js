adcb919419fff8407b1c746513a16679d43a3cfc
MattKenyon03 <165806104+mattkenyon01@users.noreply.github.com>
2025-12-01 18:14:26 +0000

    Header image fix

---
+++ js/chart.js
@@ -0,0 +1,28 @@
+﻿
+window.renderChart = function (climbers, bucket, girls) {
+    const ctx = document.getElementById('renderChart')?.getContext('2d');
+    if (!ctx) {
+        console.error("Canvas not found");
+        return;
+    }
+
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Castle Climbers', 'Bucket Connect', '
+
+'],
+            datasets: [{
+                data: [climbers, bucket, girls],
+                backgroundColor: ['#911f1f', '#41d5c3', '#eda193'],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            cutout: '65%',
+            plugins: {
+                legend: { display: false }
+            }
+        }
+    });
+};
